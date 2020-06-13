0f3862b8949fee1c575546970d81630faeadd457
Anthony Collini <collini.anthony@gmail.com>
2020-06-12 20:37:43 -0500

    Initial structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+		<div id = "drum-machine">
+			<div className = "drum-pad" id = "q">Q
+			</div>
+			<div className = "drum-pad" id = "w">W
+			</div>
+			<div className = "drum-pad" id = "e">E
+			</div>
+			<div className = "drum-pad" id = "a">A
+			</div>
+			<div className = "drum-pad" id = "s">S
+			</div>
+			<div className = "drum-pad" id = "d">D
+			</div>
+			<div className = "drum-pad" id = "z">Z
+			</div>
+			<div className = "drum-pad" id = "x">X
+			</div>
+			<div className = "drum-pad" id = "c">C
+			</div>
+			<div id = "display">
+				<h1>Hello World!</h1>
+			</div>
+		</div>
     </div>
   );
 }
